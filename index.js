c21e8d0c996d43824bcb10d89b57818cf95b016d
Alvin <wangalvin23@gmail.com>
2023-06-07 22:41:03 -0700

    add app.post capability for /api/pokemon for getting client request

--- index.js
+++ index.js
@@ -3,13 +3,22 @@ const app = express();
 const cors = require("cors");
 const pool = require("./db");
 
-(async () => {
+/*(async () => {
   const result = await pool.query("SELECT * FROM pokemon");
   console.log(result);
 })();
-
+*/
 app.use(cors());
 app.use(express.json());
+
+/*app.get("/api", (req, res) => {
+  res.json({ message: "Hello from Express!" });
+});*/
+
+app.post("/api/pokemon", (req, res) => {
+  console.log({ reqPath: "/api/pokemon", data: req.body.pokemonForm });
+  res.status(200).end();
+});
 
 app.listen(5000, () => {
   console.log("server started on port 5000");
