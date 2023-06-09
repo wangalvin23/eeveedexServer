7bdb2668ea8dcc16a0c3736bb2f385f4d622529f
Alvin <wangalvin23@gmail.com>
2023-06-08 18:39:18 -0700

    update database.sql to include all eeveelutions add database to server to client data pass using json.stringify

--- index.js
+++ index.js
@@ -3,17 +3,21 @@ const app = express();
 const cors = require("cors");
 const pool = require("./db");
 
-/*(async () => {
-  const result = await pool.query("SELECT * FROM pokemon");
-  console.log(result);
-})();
-*/
 app.use(cors());
 app.use(express.json());
 
-/*app.get("/api", (req, res) => {
-  res.json({ message: "Hello from Express!" });
-});*/
+/*(async () => {
+  const result = await pool.query("SELECT DISTINCT * FROM pokemon");
+  console.log(result);
+})();*/
+
+app.get("/api/pokemon", (req, res) => {
+  (async () => {
+    const result = await pool.query("SELECT DISTINCT * FROM pokemon");
+    console.log(result.rows);
+    res.json({ message: result.rows });
+  })();
+});
 
 app.post("/api/pokemon", (req, res) => {
   console.log({ reqPath: "/api/pokemon", data: req.body.pokemonForm });
